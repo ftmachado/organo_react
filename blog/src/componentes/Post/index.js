8d3c3c342023dbd456bc40612ca7f9c7e3526382
Fhabiana Thieli dos Santos Machado <fhabianamachado@gmail.com>
2025-05-23 09:32:45 -0300

    Posts recomendados e scrollToTop

--- blog/src/componentes/Post/index.js
+++ blog/src/componentes/Post/index.js
@@ -1,9 +1,11 @@
 import { useParams } from "react-router-dom"
 import ReactMarkDown from "react-markdown"
 import PostModelo from "../PostModelo"
+import PostCard from "../PostCard"
 import NaoEncontrada from '../../paginas/NaoEncontrada'
 import posts from '../../json/posts.json'
 import './Post.css'
+import styles from './Post.module.css'
 import PaginaPadrao from "../../paginas/PaginaPadrao"
 
 const Post = () => {
@@ -14,6 +16,12 @@ const Post = () => {
     if (!post) {
         return <NaoEncontrada />
     }
+
+    const postsRecomenados = posts
+        .filter((post) => post.id !== Number(param.id))
+        .sort((a, b) => b.id - a.id)
+        .slice((0,3))
+
     return (
         <PaginaPadrao>
             <PostModelo
@@ -25,6 +33,15 @@ const Post = () => {
                         {post.texto}
                     </ReactMarkDown>
                 </div>
+
+                <h2 className={styles.tituloOutrosPosts}>Outros posts que você pode gostar:</h2>
+                <ul className={styles.postRecomendados}>
+                    {postsRecomenados.map((post) => (
+                        <li key={post.id}>
+                            <PostCard post={post}/>
+                        </li>
+                    ))}
+                </ul>
             </PostModelo>
 
         </PaginaPadrao>
